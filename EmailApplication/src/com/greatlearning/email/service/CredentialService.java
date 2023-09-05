5bacc5926dc0147760fd8cffc62c9134e5b4afa5
Uttaraa <142016779+Uttaraa@users.noreply.github.com>
2023-09-05 18:04:36 +0530

    Update CredentialService.java
    Added Comments

--- EmailApplication/src/com/greatlearning/email/service/CredentialService.java
+++ EmailApplication/src/com/greatlearning/email/service/CredentialService.java
@@ -17,14 +17,24 @@ public class CredentialService implements ICredentialService{
 	@Override
 	public void generateEmail(Employee employee) {
 
-		String emailAddress= employee.getFirstName().toLowerCase()+  employee.getLastName().toLowerCase()+"@"+employee.getDepartment().toLowerCase()+".gl.in";
+		/* Generating the email address of new hires by using the function "toLowerCase"
+                   which will convert the entries in lower case
+		   and getDepartment function will assign the respective department in employee's email */
+
+		String emailAddress= employee.getFirstName().toLowerCase()+employee.getLastName().toLowerCase()+"@"+employee.getDepartment().toLowerCase()+".gl.in";
 		employee.setEmailAddress(emailAddress);
 
 	}
 
 	@Override
 	public void generatePassword(Employee employee) {
+		
+		//Generating eight digit password using random.nextInt() funtion by randomly choosing the characters feeded in above string "passwordChars"
+		
 		String password ="";
+		
+		//for loop for picking eight characters
+		
 		for (int i=0; i<=8; i++) {
 			password = password + passwordChars.charAt(random.nextInt(passwordChars.length()));
 			employee.setPassword(password);
@@ -34,6 +44,9 @@ public class CredentialService implements ICredentialService{
 
 	@Override
 	public void showCredentials(Employee employee) {
+		
+		//display generated credentials to the user
+		
 		System.out.println("Dear"+ employee.getFirstName()+ " your generated credentials are as follows");
 		System.out.println("Email\t\t: "+ employee.getEmailAddress());
 		System.out.println("Password\t: "+ employee.getPassword());
